4cd1c48b655e0c1ebbcdb49a6bb65c7b863eaf1e
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-10-29 09:34:15 +0530

    Add success and failure redirect for fb oauth

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,7 +3,18 @@ import passport from 'passport';
 export default app => {
   app.get('/auth/facebook', passport.authenticate('facebook'));
 
-  app.get('/auth/facebook/callback', passport.authenticate('facebook'));
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/api/login',
+    }),
+  );
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
