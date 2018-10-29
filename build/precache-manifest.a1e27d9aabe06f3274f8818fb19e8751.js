7aa485ae477558d2d3337cbaddbf12b021da169d
thisisvithu <spidyv3@gmail.com>
2018-10-29 16:44:32 +0530

    adding nav

---
+++ build/precache-manifest.a1e27d9aabe06f3274f8818fb19e8751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c299e43a35ffe66f1a99",
+    "url": "/static/js/main.c299e43a.chunk.js"
+  },
+  {
+    "revision": "695b6c768e02b4fdccc4",
+    "url": "/static/js/1.695b6c76.chunk.js"
+  },
+  {
+    "revision": "c299e43a35ffe66f1a99",
+    "url": "/static/css/main.5d848e74.chunk.css"
+  },
+  {
+    "revision": "0011ef10743b12a6811f0ab9f3f5944e",
+    "url": "/index.html"
+  }
+];
