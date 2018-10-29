996a66db1aaf8df324c7a0759ae2c5fd2132aaa1
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-10-29 09:23:26 +0530

    Add verify endpoint for authenticated users

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,4 +4,8 @@ export default app => {
   app.get('/auth/facebook', passport.authenticate('facebook'));
 
   app.get('/auth/facebook/callback', passport.authenticate('facebook'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
