e8f13965453b58b987ca141ac8c84ef0a684eb13
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-10-30 18:29:23 +0530

    Dictate styling directory structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './styles/main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+ |--------------------------------------------------------------------------
+ | Hot Module Replacement with Webpack
+ |--------------------------------------------------------------------------
+ |
+ | http://webpack.github.io/docs/hot-module-replacement-with-webpack.html
+ | https://medium.com/@dan_abramov/hot-reloading-in-react-1140438583bf
+ | http://andrewhfarmer.com/webpack-hmr-tutorial/
+ |
+ */
+const rootEl = (document.getElementById('root'));
+let render = () => {
+  const Root = require('./App').default;
+  ReactDOM.render(<Root />, rootEl);
+};
+
+if ((module).hot) {
+  const renderApp = render;
+  const renderErr = err => {
+    const Redbox = require('redbox-react');
+    ReactDOM.render(<Redbox error={err} />, rootEl);
+  };
+
+  render = () => {
+    try {
+      renderApp();
+    } catch (err) {
+      console.log(err);
+      renderErr(err);
+    }
+  };
+
+  (module).hot.accept('./App', render);
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | render
+ |--------------------------------------------------------------------------
+ |
+ | Go!
+ |
+ */
+render();
