1e470de1dc7f547db7fcc70b3f37ab714d5c0fc6
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-10-31 19:31:38 +0530

    Fix semicolon bug

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,11 +4,11 @@ export default (app) => {
 	app.get('/auth/facebook', passport.authenticate('facebook'));
 	app.get(
 		'/auth/facebook/callback',
-    passport.authenticate('facebook', {
-      session: false,
-      successRedirect: `?token=123`,
-      failureRedirect: '/'
-    })),
+		passport.authenticate('facebook', {
+			session: false,
+			successRedirect: `?token=123`,
+			failureRedirect: '/',
+		}),
 	);
 
 	app.get('/api/auth_user', (req, res) => {
