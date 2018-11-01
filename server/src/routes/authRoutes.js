046154cf1cd0bd1cf27ab9085ef326e51429ca68
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-01 05:48:29 +0530

    Update server/src/routes/authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -9,24 +9,27 @@ function tokenForUser(user) {
 export default (app) => {
 	app.get('/auth/facebook', (req, res, next) => {
 		passport.authenticate('facebook', {
-			callbackURL: `/auth/facebook/callback${req.query.redirect
-				? '?redirect=' + encodeURIComponent(req.query.redirect)
-				: ''}`,
+			callbackURL: `/auth/facebook/callback${req.params.redirect ? '/' + req.params.redirect : ''}`,
 			session: false,
 		})(req, res, next);
 	});
 
 	app.get('/auth/facebook/callback', (req, res, next) => {
 		passport.authenticate('facebook', {
-			callbackURL: `/auth/facebook/callback${req.query.redirect
-				? '?redirect=' + encodeURIComponent(req.query.redirect)
-				: ''}`,
+			callbackURL: '/auth/facebook/callback',
+			successRedirect: '/',
+			failureRedirect: '/',
 			session: false,
-		}),
-			(req, res) => {
-				console.log('req from callback', req);
-				// res.redirect(`${req.query.redirect ? req.query.redirect : '/'}?token=1234`);
-			};
+		});
+	});
+
+	app.get('/auth/facebook/callback/vote', (req, res, next) => {
+		passport.authenticate('facebook', {
+			callbackURL: '/auth/facebook/callback/vote',
+			successRedirect: '/vote',
+			failureRedirect: '/vote',
+			session: false,
+		});
 	});
 
 	app.get('/api/auth_user', (req, res) => {
