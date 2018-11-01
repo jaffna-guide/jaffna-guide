cb99ccb2bf7330a4ca50ced982fb7030b6b62469
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-01 10:48:36 +0530

    Update server/src/middlewares/requireAuth.js

--- server/src/middlewares/requireAuth.js
+++ server/src/middlewares/requireAuth.js
@@ -1,3 +1,7 @@
 import passport from 'passport';
 
 export default passport.authenticate('jwt', { session: false });
+
+// requireAuth will only guarantee that a user model is attached to req.user.
+// We might need more consise middleware to check certain properties on the user model,
+// before we allow certain actions.
