c948abd4e691559bcd5c356fe092b3fb12fc2921
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-11 19:33:15 +0800

    Install a loading spinner while authenticating protected routes

--- client/src/components/routes/AdminRoute.js
+++ client/src/components/routes/AdminRoute.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { observer, inject } from 'mobx-react';
 import { Route, Redirect } from 'react-router-dom';
-import DevTools from 'mobx-react-devtools';
+import Spinner from 'react-spinkit';
 
 @inject('AuthStore')
 @observer
@@ -26,17 +26,16 @@ class AdminRoute extends React.Component {
 				{...rest}
 				render={(props) => {
 					if (AuthStore.state === 'pending') {
-						console.log('case: pending');
-						return <div>pending|{AuthStore.state}</div>;
+						return (
+							<div className="spinner-main">
+								<Spinner className="spinner-main__spinner" name="line-scale" />
+							</div>
+						);
 					} else if (AuthStore.isAdmin) {
-						console.log('case: admin');
 						return <Component {...props} />;
 					} else if (AuthStore.isAuthenticated || AuthStore.authenticationFailed) {
-						console.log('case: authentication completed');
 						return <Redirect to="/" />;
 					} else {
-						console.log('case: redirect');
-						// return <div>redirect</div>;
 						window.location.href = `/auth/facebook?redirect=${this.props.path}`;
 					}
 				}}
