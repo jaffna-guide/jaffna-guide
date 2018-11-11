325cbd0655c16d94ac052c97358aee68945b6e46
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-11 16:05:42 +0800

    Prepare requireAdmin express middleware

--- server/src/middlewares/requireAdmin.js
+++ server/src/middlewares/requireAdmin.js
@@ -1,9 +1,18 @@
-import R from 'ramda';
+import express from 'express';
 import requireAuth from './requireAuth';
+
+const router = express.Router();
 
 const requireAdmin = (req, res, next) => {
 	console.log('req.user', req.user);
-	next();
+	if (req.user.roles.includes('admin')) {
+		next();
+	} else {
+		res.send(401);
+	}
 };
 
-export default requireAuth;
+router.use(requireAuth);
+router.use(requireAdmin);
+
+export default router;
