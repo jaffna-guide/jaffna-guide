6cafe7959dca064303cbca50dcd11ec68c4efaf4
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-12 18:37:41 +0800

    Update client/src/pages/home/LoginWelcome.js

--- client/src/pages/home/LoginWelcome.js
+++ client/src/pages/home/LoginWelcome.js
@@ -14,7 +14,8 @@ class LoginWelcome extends React.Component {
 
 	render() {
 		const { AuthStore } = this.props;
-		const username = localStorage.getItem('username');
+    const username = localStorage.getItem('username');
+    console.log('username', username);
 
 		return (
 			<div className="login-welcome">
