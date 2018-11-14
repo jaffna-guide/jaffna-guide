ee0464000ccdcdbffdb96f4a76f720d45759653d
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-14 18:33:07 +0800

    Make logo a link to the home page

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<header className="header">
-			<div className="header__combination">
+			<Link to="/" className="header__combination">
 				<span className="header__text">Jaffna</span>
 				<img className="header__logo" src="/logo-border.png" alt="Logo" />
 				<span className="header__text">Guide</span>
-			</div>
+			</Link>
 		</header>
 	);
 };
