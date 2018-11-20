1841930479182d3935659bc540bf53c305639d56
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-20 12:17:06 +0800

    Make Place model more flexible

--- server/src/models/Place.js
+++ server/src/models/Place.js
@@ -2,28 +2,31 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const markerSchema = new Schema({ default: { type: String }, active: { type: String }, orientation: { type: String } });
+const imageSchema = new Schema({ thumbnail: { type: String }, original: { type: String }, credit: { type: String } });
+
 const placeSchema = new Schema({
-  body: String,
+	body: String,
 	name: {
 		en: String,
 		ta: String,
-  },
-  description: {
-    en: String,
-    ta: String,
-  },
-  score: { type: Number, default: 0 },
+	},
+	description: {
+		en: String,
+		ta: String,
+	},
+	score: { type: Number, default: 0 },
 	latitude: Number,
-  longitude: Number,
-  active: { type: Boolean, default: true },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
-  marker: { type: String },
-  cover: { type: String },
-  images: [{ type: String }],
+	longitude: Number,
+	active: { type: Boolean, default: true },
+	category: { type: Schema.Types.ObjectId, ref: 'Category' },
+	cover: { type: String },
+	marker: markerSchema,
+	images: [ imageSchema ],
 	createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
-  updatedBy: { type: Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: Date,
+	updatedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: Date,
 });
 
 export default mongoose.model('Place', placeSchema);
