76946389fa346b420bf8b46a9c42d81463cb1c13
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-11-25 00:43:09 +0800

    Add hotels details page

--- client/src/pages/places/HotelsDetailsPage.js
+++ client/src/pages/places/HotelsDetailsPage.js
@@ -1,7 +1,50 @@
 import * as React from 'react';
+import { inject, observer } from 'mobx-react';
+import { withRouter } from 'react-router-dom';
 
-const HotelsDetailsPage = () => {
-	return <div>HotelsDetailsPage</div>;
-};
+import { Carousel } from '../../components/molecules';
+import PlacesBallot from './PlacesBallot';
+
+@withRouter
+@inject('AuthStore')
+@inject('PlaceStore')
+@observer
+class HotelsDetailsPage extends React.Component {
+	componentDidMount() {
+		const { AuthStore, match } = this.props;
+		const token = localStorage.getItem('token');
+		const { place } = match.params;
+
+		if (token) {
+			AuthStore.authenticate(place);
+		}
+	}
+
+	render() {
+		const { PlaceStore } = this.props;
+		const place = PlaceStore.currentPlace;
+
+		return (
+			<div className="hotels-details">
+				<div className="hotels-details__header">
+					<div className="hotels-details__heading">
+						<h1 className="hotels-details__title">{place.name.en}</h1>
+						<h2 className="hotels-details__subtitle">{place.name.ta}</h2>
+					</div>
+					<div className="hotels-details__ballot">
+						<PlacesBallot place={place} category="culture" />
+					</div>
+				</div>
+				<div className="hotels-details__carousel">
+					<Carousel name={place.name.en} images={place.images} />
+				</div>
+				<div className="hotels-details__description">
+					<div className="hotels-details__description--en">{place.description.en}</div>
+					<div className="hotels-details__description--ta">{place.description.ta}</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default HotelsDetailsPage;
