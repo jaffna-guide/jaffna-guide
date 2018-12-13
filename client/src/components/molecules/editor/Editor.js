aca9abd88bc23a58aae07c158ba9b1a4c952db49
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-12-13 15:49:31 +0900

    WIP: Add emoji plugin to rich text editor

--- client/src/components/molecules/editor/Editor.js
+++ client/src/components/molecules/editor/Editor.js
@@ -5,20 +5,20 @@ import createLinkifyPlugin from 'draft-js-linkify-plugin';
 import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin';
 import createSideToolbarPlugin from 'draft-js-side-toolbar-plugin';
 import createHashtagPlugin from 'draft-js-hashtag-plugin';
-import {
-  HeadlineOneButton,
-  HeadlineTwoButton,
-} from 'draft-js-buttons';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import { HeadlineOneButton, HeadlineTwoButton } from 'draft-js-buttons';
 
 const linkifyPlugin = createLinkifyPlugin();
 const mentionPlugin = createMentionPlugin();
 const sideToolbarPlugin = createSideToolbarPlugin();
 const hashtagPlugin = createHashtagPlugin();
+const emojiPlugin = createEmojiPlugin();
 
-const plugins = [ linkifyPlugin, mentionPlugin, sideToolbarPlugin, hashtagPlugin ];
+const plugins = [ linkifyPlugin, mentionPlugin, sideToolbarPlugin, hashtagPlugin, emojiPlugin ];
 
 const { MentionSuggestions } = mentionPlugin;
 const { SideToolbar } = sideToolbarPlugin;
+const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
 
 class Editor extends React.Component {
 	state = { editorState: EditorState.createEmpty(), suggestions: this.props.mentionSuggestions };
@@ -47,28 +47,32 @@ class Editor extends React.Component {
 
 	render() {
 		return (
-			<div className="editor form-input" onClick={this.focusEditor}>
-				<DraftEditor
-					ref={(element) => {
-						this.editor = element;
-					}}
-					editorState={this.state.editorState}
-					onChange={this.onChange}
-					plugins={plugins}
-				/>
-				<MentionSuggestions
-					onSearchChange={this.onSearchChange}
-					suggestions={this.state.suggestions}
-					onAddMention={this.onAddMention}
-				/>
-        <SideToolbar>
-          {(props) => (
-            <>
-              <HeadlineOneButton {...props} />
-              <HeadlineTwoButton {...props} />
-            </>
-          )}
-        </SideToolbar>
+			<div>
+				<div className="form-input editor" onClick={this.focusEditor}>
+					<DraftEditor
+						ref={(element) => {
+							this.editor = element;
+						}}
+						editorState={this.state.editorState}
+						onChange={this.onChange}
+						plugins={plugins}
+					/>
+					<MentionSuggestions
+						onSearchChange={this.onSearchChange}
+						suggestions={this.state.suggestions}
+						onAddMention={this.onAddMention}
+					/>
+					<SideToolbar>
+						{(props) => (
+							<div>
+								<HeadlineOneButton {...props} />
+								<HeadlineTwoButton {...props} />
+							</div>
+						)}
+					</SideToolbar>
+				</div>
+				<EmojiSuggestions />
+				<EmojiSelect />
 			</div>
 		);
 	}
